6520d5b64a3157aee0b51bfae49a2ea969256000
Nate Conrad <nathanielrconrad@gmail.com>
2021-12-14 14:45:09 -0500

    finalize investment growth calculator

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
 var form = document.querySelector("form");
 form.addEventListener("submit", function(event){
   event.preventDefault();
-  console.log(event.target.value);
 })
 
 function crunchSomeNumbers(){
   
   var initial = document.getElementById("initial").value;
   var additional = document.getElementById("additional").value;
-  var age = document.getElementById("age").value;
+  var age = Number(document.getElementById("age").value);
   
-  console.log(initial, additional, age);
-
   document.getElementById("initial-investment").innerHTML = initial;
   document.getElementById("additional-investment").innerHTML = additional;
   document.getElementById("inputted-age").innerHTML = age;
 
-var wealthEachYear = [];
+  var activeCompoundWealthEachYear = [];
+  var passiveCompoundWealthEachYear = [];
 
-function compoundContinuous(start, add, time){
+function compoundContinuous(start, add, time, rate){
   for (let i = 0; i < time; i++) {
-    start = start * (1 + 0.13);
-    console.log(start)
+    start = start * (1 + rate);
     start += (add * 12);
-    console.log(start)
   }
-  wealthEachYear.push(start);
-  return start;
+  if (rate === 0.13){
+  activeCompoundWealthEachYear.push(parseInt(start));
+  } else if (rate === 0.07) {
+    passiveCompoundWealthEachYear.push(parseInt(start));
+  } else {
+    console.log("Invalid rate given.");
+    return null;
+  }
+  return parseInt(start);
 }
-  console.log(wealthEachYear);
-  var compoundedContinuously = compoundContinuous(initial, additional, 10);
-  document.getElementById("compounded").innerHTML = compoundedContinuously;
 
-;}
+function howMuchOlder(years){
+  return age + years;
+};
+
+function wealthAtEachAge(rate){
+  var ages = [10, 20, 30, 40];
+  var results = [];
+  for (let i = 0; i < ages.length; i++){
+    results.push("In " + ages[i] + " years, at " + howMuchOlder(ages[i]) + ", you will have " + compoundContinuous(initial, additional, ages[i], rate) + " dollars.");
+  }
+  return results.join("<br>");
+};
+
+function postResults(type){
+  if (type==="active"){
+    document.getElementById("active-compounded").innerHTML = wealthAtEachAge(0.13);
+  } else if (type==="passive") {
+    document.getElementById("passive-compounded").innerHTML = wealthAtEachAge(0.07);
+  } else {
+    return null;
+  }
+};
+
+postResults("active");
+postResults("passive");
+
+console.log(activeCompoundWealthEachYear, passiveCompoundWealthEachYear);
+
+};
